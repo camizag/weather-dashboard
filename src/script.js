555c28902adc5c2089366c1a9b37087956b8c58c
Camila Zagal <camilazags@gmail.com>
2022-06-08 12:44:53 -0500

    made js variables

---
+++ src/script.js
@@ -0,0 +1,10 @@
+let searchQuery = document.getElementById("search-query");
+let searchBtn = document.getElementById("search-btn");
+
+let todayCard = document.getElementById("today-card");
+let time = document.getElementById("time");
+let temperature = document.getElementById("temperature");
+let windSpeed = document.getElementById("wind-speed");
+let humidity = document.getElementById("humidity");
+
+let weekCards = document.getElementById("week-cards");
